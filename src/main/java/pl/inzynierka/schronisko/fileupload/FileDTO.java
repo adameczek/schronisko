4ef6e3283599f1548189cb60290ca82cbdca433b
Adam Sawicki <adam2sawicki@gmail.com>
2023-04-15 18:34:38 +0200

    add dtos

---
+++ src/main/java/pl/inzynierka/schronisko/fileupload/FileDTO.java
@@ -0,0 +1,66 @@
+package fileupload;
+
+import jakarta.persistence.*;
+import pl.inzynierka.schronisko.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class FileDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private byte[] bytes;
+    @ManyToOne
+    private pl.inzynierka.schronisko.user.User owner;
+    private LocalDateTime createdAt;
+
+    public FileDTO(long id, byte[] bytes, User owner, LocalDateTime createdAt) {
+        this.id = id;
+        this.bytes = bytes;
+        this.owner = owner;
+        this.createdAt = createdAt;
+    }
+
+    public FileDTO(byte[] bytes, User owner, LocalDateTime createdAt) {
+        this.bytes = bytes;
+        this.owner = owner;
+        this.createdAt = createdAt;
+    }
+
+    public FileDTO() {
+
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/pl/inzynierka/schronisko/fileupload/ImageFileDTO.java
@@ -0,0 +1,38 @@
+package fileupload;
+
+import pl.inzynierka.schronisko.user.User;
+
+import java.time.LocalDateTime;
+
+public class ImageFileDTO extends FileDTO {
+    private byte[] smallBytes;
+    private byte[] mediumBytes;
+
+    public ImageFileDTO(long id, byte[] bytes, User owner, LocalDateTime createdAt, byte[] smallBytes, byte[] mediumBytes) {
+        super(id, bytes, owner, createdAt);
+        this.smallBytes = smallBytes;
+        this.mediumBytes = mediumBytes;
+    }
+
+    public ImageFileDTO(byte[] bytes, User owner, LocalDateTime createdAt, byte[] smallBytes, byte[] mediumBytes) {
+        super(bytes, owner, createdAt);
+        this.smallBytes = smallBytes;
+        this.mediumBytes = mediumBytes;
+    }
+
+    public byte[] getSmallBytes() {
+        return smallBytes;
+    }
+
+    public void setSmallBytes(byte[] smallBytes) {
+        this.smallBytes = smallBytes;
+    }
+
+    public byte[] getMediumBytes() {
+        return mediumBytes;
+    }
+
+    public void setMediumBytes(byte[] mediumBytes) {
+        this.mediumBytes = mediumBytes;
+    }
+}
