b6c4199724cd7eb3193a76735da6bf1598fd3d20
Adam Sawicki <adam2sawicki@gmail.com>
2023-04-19 21:13:35 +0200

    create add_all_roles.sh script

--- add_all_roles.sh
+++ add_all_roles.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 POSTGRES_ID=$(docker ps -aqf "name=postgres")
 echo $POSTGRES_ID
-docker exec $POSTGRES_ID psql -U myuser mydb -c "UPDATE users SET roles = '{0, 1, 2, 3}' WHERE email = '$2';"
+docker exec $POSTGRES_ID psql -U myuser mydb -c "UPDATE users SET roles = '{0, 1, 2, 3}' WHERE email = '$1';"
